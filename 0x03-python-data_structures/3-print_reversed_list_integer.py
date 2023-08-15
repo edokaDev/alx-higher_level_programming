f1bc164099a96321a36d2f897d932e28b3e41d07
edoka <peter.edoka.242464@unn.edu.ng>
2023-08-15 02:53:03 +0100

    reverse list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,8 @@ def print_reversed_list_integer(my_list=[]):
     if my_list:
         length = len(my_list)
 
-        rev = my_list[length-1:-1]
+        rev = my_list[length-1::-1]
 
         for i in rev:
             print("{:d}".format(i))
+
