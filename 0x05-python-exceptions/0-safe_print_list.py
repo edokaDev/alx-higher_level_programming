a2ed2c0f49c8f84458c5f5d9d4d54baa2ba54160
edoka <peter.edoka.242464@unn.edu.ng>
2023-08-28 13:57:12 +0100

    added a printed variable

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    printed = 0
     try:
         for i in range(x):
             print(my_list[i], end='')
-            i += 1
+            printed += 1
     except IndexError:
         pass
     print()
-    return (i)
+    return (printed)
