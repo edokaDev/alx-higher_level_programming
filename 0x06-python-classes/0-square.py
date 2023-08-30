097b0eecb70f756c0011a28b08204ccba6511ae1
edoka <peter.edoka.242464@unn.edu.ng>
2023-08-30 01:55:54 +0100

    my first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+"""Empty Square Script.
+
+This script defines a Square.
+"""
+
+
+class Square():
+    """Square Class.
+
+    This class defines and empty square.
+    """
+
+    def __init__(self):
+        """Init method.
+
+        This is the constructor method.
+        """
+        pass
