4c023e99939eae29d06721b247a5a681b69552b6
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-05 00:31:18 +0100

    simple rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Empty Rectangle Module.
+
+This script defines a Rectangle.
+"""
+
+
+class Rectangle():
+    """Rectangle Class.
+
+    This class defines and empty Rectangle.
+    """
+
+    pass
