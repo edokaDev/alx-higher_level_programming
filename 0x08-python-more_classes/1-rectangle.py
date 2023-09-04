4bf2045126aa9d60ea2b27830f9262addc18af7a
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-05 00:55:22 +0100

    defn of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,25 +12,16 @@ class Rectangle():
     """
 
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
-
-    @property
-    def width(self):
-        return self.__width
+        self.__width = width
 
     @property
     def height(self):
         return self.__height
 
-    @width.setter
-    def width(self, value):
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-
-        self.__width = value
+    @property
+    def width(self):
+        return self.__width
 
     @height.setter
     def height(self, value):
@@ -40,3 +31,12 @@ class Rectangle():
             raise ValueError("height must be >= 0")
 
         self.__height = value
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
