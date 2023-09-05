800b47d1e063edb17c27da62b96f0351c5852113
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-05 01:41:30 +0100

    eval is magic

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+"""Rectangle Module.
+
+This module is based on the `3-rectangle` module. \
+    As an improvement, it implements repl() method \
+    which returns the string representation of the Rectangle.
+"""
+
+
+class Rectangle:
+    """Rectangle Class.
+
+    This class defines a Rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """Initialize Instance.
+
+        This method initializes a rectangle instance.
+
+        Args:
+            width(int): width of rectangle.
+            height(int): height of rectangle.
+        """
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        Getter method for rectangle height.
+
+        Returns:
+        int: The height of the rectangle.
+        """
+        return self.__height
+
+    @property
+    def width(self):
+        """
+        Getter method for rectangle width.
+
+        Returns:
+        int: The width of the rectangle.
+        """
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter method for rectangle height.
+
+        Args:
+            value(int): new height
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter method for rectangle width.
+
+        Args:
+            value(int): new width
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is < 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    def area(self):
+        """Area of the Rectangle.
+
+        This calculates the area of the rectangle.
+
+        Return:
+            the area.
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        """Perimeter of the Rectangle.
+
+        This calculates the perimeter of the rectangle.
+
+        Return:
+            the perimeter or zero if either width of height is 0.
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        """Representation of the rectangle.
+
+        This prints # according to the perimeter of the rectangle.
+        """
+        if self.perimeter() > 0:
+            for i in range(self.height - 1):
+                print("#" * self.width)
+            print("#" * self.width, end="")
+        return ""
+
+    def __repr__(self):
+        """Formal representation of the rectangle.
+
+        This returns a string representation of the rectangle
+        to be able to recreate a new instance by using eval()
+        """
+        return f"Rectangle({self.width}, {self.height})"
