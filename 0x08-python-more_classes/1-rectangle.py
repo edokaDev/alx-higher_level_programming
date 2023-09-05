2dc86afc352680c96520c6f36927594bea425c3e
edoka <peter.edoka.242464@unn.edu.ng>
2023-09-05 01:13:35 +0100

    hot fix 2

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
             TypeError: If value is not an integer.
             ValueError: If value is < 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -74,7 +74,7 @@ class Rectangle:
             TypeError: If value is not an integer.
             ValueError: If value is < 0.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
