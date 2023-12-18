313d0d7e56a199acc24ecafe64a5e68257ff2ea6
edoka <peter.edoka.242464@unn.edu.ng>
2023-12-18 03:45:21 +0100

    city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""City Model.
+
+Class definition of a City.
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """schema for the cities table."""
+
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
