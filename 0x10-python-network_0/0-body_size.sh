af9cbd92b073f60c33aa26be63667f9ce7db7f5f
edoka <peter.edoka.242464@unn.edu.ng>
2024-01-09 13:27:27 +0100

    minimalistic script

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # a script that displays the size of a curl response.
-
-if [ -z "$1" ]; then
-	exit 1
-fi
-
-url="$1"
-response=$(curl -sI "$url")
-cl=$(echo "$response" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r')
-
-if [ -n "$cl" ]; then
-	echo "$cl"
-	exit 0
-fi
-exit 1
+curl -sI "$1" | grep -i '^Content-Length:' | awk '{print $2}' | tr -d '\r'
