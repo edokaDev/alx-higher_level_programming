3286f057e11ecb5933bb8d227f2ff130309cc007
edoka <peter.edoka.242464@unn.edu.ng>
2024-01-10 13:44:38 +0100

    fix typo error

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a post request with parameters.
-curl -s -X POST -d "email=test@gmail.com" -d "subect=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
